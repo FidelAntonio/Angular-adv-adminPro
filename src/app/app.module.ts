6271f84e5800dc5bab7736169cf68396061fcabf
FidelAntonio <66265012+FidelAntonio@users.noreply.github.com>
2021-06-29 23:33:06 -0500

    fin de seccion 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+//MODULOS ECHOS POR MI
 import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module';
+import { AuthModule } from './auth/auth.module';
+
 
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProgressComponent,
-    Grafica1Component,
-    DashboardComponent,
     NopagefoundComponent,
-    PagesComponent,
-    LoginComponent,
-    RegisterComponent
+    
+  
   ],
   imports: [
     BrowserModule,
-    SharedModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
